62e9ae89fca8723c7915f6d7d4e08096b66a073b
Vicent Marqués <vicentgrafic@yahoo.es>
2016-01-27 18:18:08 +0100

    commit0

---
+++ src/main/main.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+
+#variables
+cadena1="hola1";
+cadena2="hola2";
+x=1;
+y=1.0;
+z=[9,8,7,6,5,4,3,2,1,0];
+
+print cadena1+","+cadena2;
+print str(x).zfill(3);
+print str(x+y).zfill(5);
+print format(x+y,'.3f').zfill(8);
